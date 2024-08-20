083ddb14d9772d16b4fb5f8424550cad5fcc23d1
Mathieu Moretti <mormmorm@hotmail.com>
2024-08-19 23:51:11 -0400

    First foray into gameserver

---
+++ gameserver.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/hexagun/gameserver/websocket"
+)
+
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("GO Websockets")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8099", nil))
+}
